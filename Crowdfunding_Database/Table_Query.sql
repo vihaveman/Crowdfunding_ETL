a6e7d840301ff991122236addd59ebec8c895e47
Violetta Haveman <120049344+vihaveman@users.noreply.github.com>
2023-05-16 00:54:48 -0400

    Create Table_Query.sql

---
+++ Crowdfunding_Database/Table_Query.sql
@@ -0,0 +1,16 @@
+/*Campaign Query*/
+SELECT *
+	FROM public."Campaign";
+	
+
+/*Category Query*/	
+SELECT *
+	FROM public."Category";
+
+/*Contacts Query*/	
+SELECT *
+	FROM public."Contacts";
+	
+/*Subcategory Query*/	
+SELECT *
+	FROM public."Subcategory";	
